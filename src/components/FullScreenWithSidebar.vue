17378fdba17f4a76b370160f86b067335739fe40
Jeffersah <greatnate1210@gmail.com>
2025-03-08 10:52:33 -0500

    tmp

---
+++ src/components/FullScreenWithSidebar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="full-container">
+        <div class="full-content">
+            <slot name="content">
+            </slot>
+        </div>
+        <div class="sidebar-content">
+            <slot name="sidebar">
+            </slot>
+        </div>
+    </div>
+</template>
